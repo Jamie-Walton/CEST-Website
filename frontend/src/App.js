0c86e78823236fe1de9f089378831699fdfacde2
Jamie Walton <jamiewalton@berkeley.edu>
2023-03-12 11:05:49 -0700

    Set up website infrastructure

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Analyze from './components/Analyze';
+
+function App() {
+  return (
+   <main>
+      {<Analyze/>}
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Analyze.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+
+class Analyze extends React.Component {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+          
+      }
+    }
+
+}
+
+export default (Analyze);
