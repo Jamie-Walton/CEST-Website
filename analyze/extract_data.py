Fix image scaling bug

analyze/extract_data.py
@@ -5,7 +5,7 @@ from pydicom import dcmread
 import os
 
 
-def load_data(identifier): #TODO: Add image sorting?
+def load_data(identifier):
         '''
         Create a 4D matrix (with dimensions x, y, offset, slice) of a previously uploaded 
         dataset and a list of its corresponding offset values.
@@ -18,22 +18,39 @@ def load_data(identifier): #TODO: Add image sorting?
         offset_num = -1
         for f in directory:
             ds = dcmread(os.path.join(MEDIA_ROOT, f'uploads/{identifier}/{f[:-4]}.dcm'))
-            if int(f.split('-')[-1]) == 1:
+            protocol_name = ds['ProtocolName'].value.split('_')
+            ppm_index = protocol_name.index("ppm")
+            if int(f[:-4].split('-')[-1]) == 1:
                 offset_num += 1
-                data[offset_num] = [ds.pixel_array]
-                offsets += int(protocol_name[ppm_index - 1])
+                data += [[ds.pixel_array]]
+                try:
+                    offsets += [float(protocol_name[ppm_index - 1])]
+                except:
+                     offsets += [0]
             else:
                 data[offset_num].append(ds.pixel_array)
-            protocol_name = ds["Protocol Name"].split('_')
-            ppm_index = protocol_name.index("ppm")
             
         return data, offsets
 
 
 def pointsToMask(poly_verts_list, nx, ny):
-        '''Convert a list of polygon vertices to a list of masks'''
+        '''Convert a list of polygon vertices to a list of masks
+
+        >>> vertices = [[
+                [207.40625, 229.203125],
+                [272.40625, 165.203125],
+                [345.40625, 238.203125],
+                [276.40625, 302.203125],
+                [198.40625, 283.203125]
+               ]]
+        >>> result = pointsToMask(vertices, 500, 500)
+        >>> f = open("pointsToMaskExample.txt")
+        >>> correct = f.read()
+        >>> result == correct
+        True
+        '''
         
-        nx, ny = int(nx), int(ny)
+        nx, ny = int(nx), int(ny) # TODO: Figure out how to address small ROI size
         x, y = np.meshgrid(np.arange(nx), np.arange(ny))
         x, y = x.flatten(), y.flatten()
         points = np.vstack((x, y)).T
@@ -44,7 +61,8 @@ def pointsToMask(poly_verts_list, nx, ny):
 
         for poly_verts in poly_verts_list:
         
-            path = Path(poly_verts)
+            scaled_verts = [[v[0] * 0.25, v[1] * 0.25] for v in poly_verts]
+            path = Path(scaled_verts)
             # Check if points are inside the polygon
             poly_mask = path.contains_points(points)
             grid = np.logical_or(grid, poly_mask)

analyze/views.py
@@ -1,23 +1,19 @@
-import numpy as np
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from django.http.response import JsonResponse
 from backend.settings import MEDIA_ROOT
-from .tools.berkeley.resources.cest_analysis import zspec_voxel, zspec_avg, plt_avg
 
+import numpy as np
 from pydicom import dcmread
 from analyze.pydicom_PIL import get_PIL_image
 from PIL import Image
-
 import secrets, string
 import os
 
 from analyze.models import Dataset, File
 from analyze.extract_data import load_data, pointsToMask
-from analyze.analyze_data import create_spectrum_roi
-from analyze.tools.berkeley.resources.b0_correction import b0_correction, interpolate_wassr, wassr_b0_map
+from analyze.analyze_data import generate_zspec, b0_correction
 
 
 class UploadView(APIView):
@@ -51,44 +47,46 @@ class UploadView(APIView):
         dataset.image_height = height
         dataset.save()
         
-        return JsonResponse({'images': images})
+        return JsonResponse({'images': images, 'width': width, 'height': height})
 
 
 @api_view(('POST',))
 def report(request):
-
     
     if request.method == 'POST':
 
+        # Retrieve dataset
         identifier = request.data["id"]
         ds = Dataset.objects.get(identifier=identifier)
         width, height = ds.image_width, ds.image_height
-        data, cest_offsets = load_data(identifier)
+        data, freq_offsets = load_data(identifier)
+        reference_frequency = 1000
 
-        epi_rois = request.data["epiROIs"]
-        epi_points = [[roi["points"]] for roi in epi_rois]
-
-        endo_rois = request.data["endoROIs"]
-        endo_points = [[roi["points"]] for roi in endo_rois]
-
-        arv_rois = request.data["arvs"]
-        arv_points = [[roi["points"]] for roi in arv_rois]
-
-        irv_rois = request.data["irvs"]
-        irv_points = [[roi["points"]] for roi in irv_rois]
-
+        # Unpack data from frontend
+        epi_points = [[roi["points"]] for roi in request.data["epiROIs"]]
+        endo_points = [[roi["points"]] for roi in request.data["endoROIs"]]
+        arv_points = [[roi["points"]] for roi in request.data["arvs"]]
+        irv_points = [[roi["points"]] for roi in request.data["irvs"]]
         pixel_wise = request.data["pixelWise"]
+        epis = [pointsToMask(p, width, height).astype(int) for p in epi_points],
+        endos = [pointsToMask(p, width, height).astype(int) for p in endo_points], 
+        arvs = [pointsToMask(p, width, height).astype(int) for p in arv_points],
+        irvs = [pointsToMask(p, width, height).astype(int) for p in irv_points]
         
-        masks = {
-            "epi": [pointsToMask(p, width, height) for p in epi_points], 
-            "endo": [pointsToMask(p, width, height) for p in endo_points], 
-            "arvs": [pointsToMask(p, width, height) for p in arv_points],
-            "irvs": [pointsToMask(p, width, height) for p in irv_points]
-        }
+        # Create myocardium mask
+        masks = [np.subtract(epi, endo) for epi, endo in zip(epis[0], endos[0])]
 
-        wassr_offsets_interp, wassr_zspecs_interp = interpolate_wassr(wassr_offsets, wassr_zspecs)
-        b0_map = wassr_b0_map(wassr_offsets_interp, wassr_zspecs_interp)
-        cest_zspecs = b0_correction(cest_offsets, data, b0_map)
-        cest_roi, map_mask = create_spectrum_roi(data, cest_zspecs)
+        # Sort images by frequency
+        data = [d for (d, f) in sorted(zip(data, freq_offsets), key=lambda tup : tup[1])]
+
+        # Generate z-spectra
+        zspec, signal_mean, signal_std, signal_n, signal_intensities, indices = \
+            generate_zspec(data, masks, arvs, irvs) # may need to fix dimensions of data?
+
+        # TODO: B0 Correction
+        x_corrected, b0_shift = b0_correction(freq_offsets[1:], zspec)
+
+        # TODO: Lorentzian Fitting
+        # TODO: Package Results
 
         return JsonResponse({})